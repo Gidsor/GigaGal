8769194af9f54f7ddb87e2942df4bca23853228b
Vadim Denisov <gidsor@gmail.com>
2018-08-06 21:59:45 +0300

    added sprites and constants

--- core/src/com/gidsor/gigagal/GameplayScreen.java
+++ core/src/com/gidsor/gigagal/GameplayScreen.java
@@ -1,4 +1,17 @@
 package com.gidsor.gigagal;
 
-public class GameplayScreen {
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.gidsor.gigagal.util.Assests;
+
+public class GameplayScreen extends ScreenAdapter {
+    private static final String TAG = GameplayScreen.class.getName();
+
+    SpriteBatch batch;
+    ExtendViewport viewport;
+
+    @Override
+    public void show() {
+    }
 }
--- core/src/com/gidsor/gigagal/Level.java
+++ core/src/com/gidsor/gigagal/Level.java
@@ -1,4 +1,11 @@
 package com.gidsor.gigagal;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Level {
+    public Level() {}
+
+    public void update(float dt) {}
+
+    public void render(SpriteBatch sb) {}
 }
