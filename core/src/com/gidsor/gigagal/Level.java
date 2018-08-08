92c0ae0372ff56db814a5ef2080797d486544ba3
Vadim Denisov <gidsor@gmail.com>
2018-08-08 13:06:40 +0300

    More platforms

--- core/src/com/gidsor/gigagal/Level.java
+++ core/src/com/gidsor/gigagal/Level.java
@@ -36,5 +36,11 @@ public class Level {
         platforms.add(new Platform(75, 90, 100, 65));
         platforms.add(new Platform(35, 55, 50, 20));
         platforms.add(new Platform(10, 20, 20, 9));
+        platforms.add(new Platform(100, 110, 30, 9));
+        platforms.add(new Platform(200, 130, 30, 40));
+        platforms.add(new Platform(150, 150, 30, 9));
+        platforms.add(new Platform(150, 180, 30, 9));
+        platforms.add(new Platform(200, 200, 9, 9));
+        platforms.add(new Platform(280, 100, 30, 9));
     }
 }
